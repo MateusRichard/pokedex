0f38f35a41cdade517aa734de9639b86a861f3cc
MateusRichard <106836298+MateusRichard@users.noreply.github.com>
2023-04-10 20:48:09 -0300

    previous and next button

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     if (APIResponse.status == 200) {
         const data = await APIResponse.json();
@@ -22,12 +25,14 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if (data) {
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.id != 555 ? ' - ' + data.name : ' - ' + data.species.name; //operador ternário
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-
         input.value = '';
+        searchPokemon = data.id;
     } else {
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not found :c';
         pokemonNumber.innerHTML = '';
     }
@@ -36,7 +41,19 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    renderPokemon(input.value);
-})
+    renderPokemon(input.value.toLowerCase());
+});
 
-renderPokemon('1');
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
